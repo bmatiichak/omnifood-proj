564ac116126664e23b6221f24d61a353c0c1a785
bmatiichak <bmatiychak@gmail.com>
2021-04-03 21:39:16 +0300

    added animations on scroll + responsive sticky navigation

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -65,7 +65,47 @@ $(document).ready(function () {
 		}
 	});
 
+    /* Animation on scroll */
 
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+    } , {
+        offset: '50%'
+    });
 
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+    } , {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+    } , {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animate__animated animate__pulse');
+    } , {
+        offset: '50%'
+    });
+
+    /* Mobile Navigation */
+
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon ion-icon');
+
+        nav.slideToggle(200);
+
+        if (icon.hasClass('menu-pop')) {
+            icon.addClass('close-outline');
+            icon.removeClass('menu-pop');
+        } else {
+            icon.addClass('menu-pop');
+            icon.removeClass('close-outline');
+        }
+    });
 
 });
